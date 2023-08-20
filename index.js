a8bba35c07f7e325f89a3c0c095c4ed7622249a9
Nicolas Caceres Latorre <nicolas.programador@gmail.com>
2023-08-19 21:27:33 -0400

    actualizados ok

--- index.js
+++ index.js
@@ -1,172 +1,236 @@
 const { pool } = require("./dataBase.js");
 
-const getEstudiantes = async() => {
-     try{
+// const getEstudiantes = async() => {
+//      try{
 
-         const res = await pool.query("SELECT * FROM estudiantes");
-         console.table(res.rows);
+//          const res = await pool.query("SELECT * FROM estudiantes");
+//          console.table(res.rows);
 
-     }catch(error){
-         console.error(error);
-     }
- }
+//      }catch(error){
+//          console.error(error);
+//      }
+//  }
 
 // getEstudiantes();
 
-const getCursos = async() => {
-    try{
+// const getCursos = async() => {
+//     try{
 
-        const res = await pool.query("SELECT * FROM cursos");
-        console.table(res);
+//         const res = await pool.query("SELECT * FROM cursos");
+//         console.table(res);
 
-    }catch(error){
-        console.error(error);
-    }
-}
+//     }catch(error){
+//         console.error(error);
+//     }
+// }
 
-const insertarEstudiante = async() => {
-    const [nombres,apellidos,edad,nro_identificacion] = process.argv.slice(2);
+// const insertarEstudiante = async() => {
+//     const [nombres,apellidos,edad,nro_identificacion] = process.argv.slice(2);
 
-    try{
+//     try{
         
-        const res = await pool.query(
-            "INSERT INTO estudiantes(nombres,apellidos,edad,nro_identificacion) values($1,$2,$3,$4)",
-            [nombres,apellidos,edad,nro_identificacion]
-        );
-        console.log(`Se ha agregado con exito a ${nombres} ${apellidos}`);
+//         const res = await pool.query(
+//             "INSERT INTO estudiantes(nombres,apellidos,edad,nro_identificacion) values($1,$2,$3,$4)",
+//             [nombres,apellidos,edad,nro_identificacion]
+//         );
+//         console.log(`Se ha agregado con exito a ${nombres} ${apellidos}`);
 
-    }catch(error){
-        switch(error.code){
-            case '42601':
-                console.log("\n ERROR! \n Error de Sintaxis\n");
-                break;
-            case '25P01':
-                console.log("\n ERROR! \n Password inválido\n");
-                break;
-            case '08003':
-                console.log("\n ERROR! \n Conexion intexistente\n");
-                break;
-            case '08006':
-                console.log("\n ERROR! \n Conexion falló\n");
-                break;
-            case '2F002':
-                console.log("\n ERROR! \n Sin permisos para modificar\n");
-                break;
-            case '57P03':
-                console.log("\n ERROR! \n Error en la conexion\n");
-                break;
-            case '42501':
-                console.log("\n ERROR! \n Privilegios insuficientes\n");
-                break;
-            case '42602':
-                console.log("\n ERROR! \n Nombre inválido\n");
-                break;
-            case '42622':
-                console.log("\n ERROR! \n Nombre demasiado largo\n");
-                break;
-            case '42939':
-                console.log("\n ERROR! \n Nombre reservado\n");
-                break;
-            case '42703':
-                console.log("\n ERROR! \n Columna indefinida\n");
-                break;
-            case '42000':
-                console.log("\n ERROR! \n Syntaxis error o violacion de acceso\n");
-                break;
-            case '42P01':
-                console.log("\n ERROR! \n Tabla no definida\n");
-                break;
-            case '42P02':
-                console.log("\n ERROR! \n Parametro no definida\n");
-                break;
-            default:
-                console.error(error.stack);
-                console.log(error.code);
-        }
-    }
-    finally{
-        console.log('Inserción Terminada');
-    }
-}
+//     }catch(error){
+//         switch(error.code){
+//             case '42601':
+//                 console.log("\n ERROR! \n Error de Sintaxis\n");
+//                 break;
+//             case '25P01':
+//                 console.log("\n ERROR! \n Password inválido\n");
+//                 break;
+//             case '08003':
+//                 console.log("\n ERROR! \n Conexion intexistente\n");
+//                 break;
+//             case '08006':
+//                 console.log("\n ERROR! \n Conexion falló\n");
+//                 break;
+//             case '2F002':
+//                 console.log("\n ERROR! \n Sin permisos para modificar\n");
+//                 break;
+//             case '57P03':
+//                 console.log("\n ERROR! \n Error en la conexion\n");
+//                 break;
+//             case '42501':
+//                 console.log("\n ERROR! \n Privilegios insuficientes\n");
+//                 break;
+//             case '42602':
+//                 console.log("\n ERROR! \n Nombre inválido\n");
+//                 break;
+//             case '42622':
+//                 console.log("\n ERROR! \n Nombre demasiado largo\n");
+//                 break;
+//             case '42939':
+//                 console.log("\n ERROR! \n Nombre reservado\n");
+//                 break;
+//             case '42703':
+//                 console.log("\n ERROR! \n Columna indefinida\n");
+//                 break;
+//             case '42000':
+//                 console.log("\n ERROR! \n Syntaxis error o violacion de acceso\n");
+//                 break;
+//             case '42P01':
+//                 console.log("\n ERROR! \n Tabla no definida\n");
+//                 break;
+//             case '42P02':
+//                 console.log("\n ERROR! \n Parametro no definida\n");
+//                 break;
+//             default:
+//                 console.error(error.stack);
+//                 console.log(error.code);
+//         }
+//     }
+//     finally{
+//         console.log('Inserción Terminada');
+//     }
+// }
 
 //insertarEstudiante();
 
-const insertarCurso = async() => {
-    const [titulo,descripcion] = process.argv.slice(2);
+// const insertarCurso = async() => {
+//     const [titulo,descripcion] = process.argv.slice(2);
 
+//     try{
+        
+//         const res = await pool.query(
+//             "INSERT INTO cursos(titulo,descripcion) values($1,$2)",
+//             [titulo,descripcion]
+//         );
+//         console.log(`Se ha agregado con exito a ${titulo} ${descripcion}`);
+
+//     }catch(error){
+//         switch(error.code){
+//             case '42601':
+//                 console.log("\n ERROR! \n Error de Sintaxis\n");
+//                 break;
+//             case '25P01':
+//                 console.log("\n ERROR! \n Password inválido\n");
+//                 break;
+//             case '08003':
+//                 console.log("\n ERROR! \n Conexion intexistente\n");
+//                 break;
+//             case '08006':
+//                 console.log("\n ERROR! \n Conexion falló\n");
+//                 break;
+//             case '2F002':
+//                 console.log("\n ERROR! \n Sin permisos para modificar\n");
+//                 break;
+//             case '57P03':
+//                 console.log("\n ERROR! \n Error en la conexion\n");
+//                 break;
+//             case '42501':
+//                 console.log("\n ERROR! \n Privilegios insuficientes\n");
+//                 break;
+//             case '42602':
+//                 console.log("\n ERROR! \n Nombre inválido\n");
+//                 break;
+//             case '42622':
+//                 console.log("\n ERROR! \n Nombre demasiado largo\n");
+//                 break;
+//             case '42939':
+//                 console.log("\n ERROR! \n Nombre reservado\n");
+//                 break;
+//             case '42703':
+//                 console.log("\n ERROR! \n Columna indefinida\n");
+//                 break;
+//             case '42000':
+//                 console.log("\n ERROR! \n Syntaxis error o violacion de acceso\n");
+//                 break;
+//             case '42P01':
+//                 console.log("\n ERROR! \n Tabla no definida\n");
+//                 break;
+//             case '42P02':
+//                 console.log("\n ERROR! \n Parametro no definida\n");
+//                 break;
+//             default:
+//                 console.error(error.stack);
+//                 console.log(error.code);
+//         }
+//     }
+//     finally{
+//         console.log('Inserción Terminada');
+//     }    
+// }
+
+// const editarEstudiante = async() => {
+//     const [nombres,id_estudiante] = process.argv.slice(2);    
+//     try{
+        
+//         const res = await pool.query(
+//             "UPDATE estudiantes SET nombres = $1 WHERE id_estudiante = $2",
+//             [nombres,id_estudiante]
+//         );
+//         console.log(`Se ha editado con exito a ${nombres}`);
+
+//     }catch(error){
+//         switch(error.code){
+//             case '42601':
+//                 console.log("\n ERROR! \n Error de Sintaxis\n");
+//                 break;
+//             case '25P01':
+//                 console.log("\n ERROR! \n Password inválido\n");
+//                 break;
+//             case '08003':
+//                 console.log("\n ERROR! \n Conexion intexistente\n");
+//                 break;
+//             case '08006':
+//                 console.log("\n ERROR! \n Conexion falló\n");
+//                 break;
+//             case '2F002':
+//                 console.log("\n ERROR! \n Sin permisos para modificar\n");
+//                 break;
+//             case '57P03':
+//                 console.log("\n ERROR! \n Error en la conexion\n");
+//                 break;
+//             case '42501':
+//                 console.log("\n ERROR! \n Privilegios insuficientes\n");
+//                 break;
+//             case '42602':
+//                 console.log("\n ERROR! \n Nombre inválido\n");
+//                 break;
+//             case '42622':
+//                 console.log("\n ERROR! \n Nombre demasiado largo\n");
+//                 break;
+//             case '42939':
+//                 console.log("\n ERROR! \n Nombre reservado\n");
+//                 break;
+//             case '42703':
+//                 console.log("\n ERROR! \n Columna indefinida\n");
+//                 break;
+//             case '42000':
+//                 console.log("\n ERROR! \n Syntaxis error o violacion de acceso\n");
+//                 break;
+//             case '42P01':
+//                 console.log("\n ERROR! \n Tabla no definida\n");
+//                 break;
+//             case '42P02':
+//                 console.log("\n ERROR! \n Parametro no definida\n");
+//                 break;
+//             default:
+//                 console.error(error.stack);
+//                 console.log(error.code);
+//         }
+//     }
+//     finally{
+//         console.log('Edición de estudiante Terminada');
+//     }
+// }
+
+//editarEstudiante();
+
+const editarCurso = async() => {
+    const [titulo,id_curso] = process.argv.slice(2);    
     try{
         
         const res = await pool.query(
-            "INSERT INTO cursos(titulo,descripcion) values($1,$2)",
-            [titulo,descripcion]
+            "UPDATE cursos SET titulo = $1 WHERE id_curso = $2",
+            [titulo,id_curso]
         );
-        console.log(`Se ha agregado con exito a ${titulo} ${descripcion}`);
-
-    }catch(error){
-        switch(error.code){
-            case '42601':
-                console.log("\n ERROR! \n Error de Sintaxis\n");
-                break;
-            case '25P01':
-                console.log("\n ERROR! \n Password inválido\n");
-                break;
-            case '08003':
-                console.log("\n ERROR! \n Conexion intexistente\n");
-                break;
-            case '08006':
-                console.log("\n ERROR! \n Conexion falló\n");
-                break;
-            case '2F002':
-                console.log("\n ERROR! \n Sin permisos para modificar\n");
-                break;
-            case '57P03':
-                console.log("\n ERROR! \n Error en la conexion\n");
-                break;
-            case '42501':
-                console.log("\n ERROR! \n Privilegios insuficientes\n");
-                break;
-            case '42602':
-                console.log("\n ERROR! \n Nombre inválido\n");
-                break;
-            case '42622':
-                console.log("\n ERROR! \n Nombre demasiado largo\n");
-                break;
-            case '42939':
-                console.log("\n ERROR! \n Nombre reservado\n");
-                break;
-            case '42703':
-                console.log("\n ERROR! \n Columna indefinida\n");
-                break;
-            case '42000':
-                console.log("\n ERROR! \n Syntaxis error o violacion de acceso\n");
-                break;
-            case '42P01':
-                console.log("\n ERROR! \n Tabla no definida\n");
-                break;
-            case '42P02':
-                console.log("\n ERROR! \n Parametro no definida\n");
-                break;
-            default:
-                console.error(error.stack);
-                console.log(error.code);
-        }
-    }
-    finally{
-        console.log('Inserción Terminada');
-    }    
-}
-
-const editarEstudiante = async() => {
-    const [id_estudiante,nombres] = process.argv.slice(2);
-    
-
-    try{
-        
-        const res = await pool.query(
-            "UPDATE estudiantes SET id_estudiante = $1 WHERE nombres = $2",
-            [id_estudiante,nombres]
-        );
-        console.log(`Se ha editado con exito a ${nombres}`);
+        console.log(`Se ha editado con exito a ${titulo}`);
 
     }catch(error){
         switch(error.code){
@@ -222,8 +286,7 @@ const editarEstudiante = async() => {
     }
 }
 
-editarEstudiante();
-
+editarCurso();
 //insertarCurso();
 
 
